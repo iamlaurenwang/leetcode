0b6ca52e75e590af138385d6610df46979191230
iamlaurenwang <iamlaurenwang@gmail.com>
2025-02-13 11:23:44 +0800

    review: 876 middle of the linked list

---
+++ the-leetcode-beginners-guide/876.js
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} head
+ * @return {ListNode}
+ */
+
+
+class ListNode{
+    constructor(val, next = null){
+        this.val = val
+        this.next = next
+    }
+}
+
+
+
+var middleNode = function(head) {
+    let listLen = 0
+    let curr = head
+
+    while(curr && curr.next !== null){
+        listLen += 1
+        curr = curr.next
+    }
+
+    let mid = Math.floor(listLen / 2)
+    curr = head
+    for(let i = 0; i < mid; i++){
+        curr = curr.next
+    }
+
+    return curr
+};
+
+// concept: 找出中間點 + override head value
+
+const h1 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5)))))
+console.log(middleNode(h1))
+
+const h2 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))))
+console.log(middleNode(h2))
