67112343f5fcbb8a85923b22cffe5bdb45a0f17c
iamlaurenwang <iamlaurenwang@gmail.com>
2025-02-13 11:33:25 +0800

    876: two pointers

--- the-leetcode-beginners-guide/876.js
+++ the-leetcode-beginners-guide/876.js
@@ -45,3 +45,20 @@ console.log(middleNode(h1))
 
 const h2 = new ListNode(1, new ListNode(2, new ListNode(3, new ListNode(4, new ListNode(5, new ListNode(6))))))
 console.log(middleNode(h2))
+
+
+var middleNodeTwoPointers = function(head){
+    let slow = head
+    let fast = head
+
+    // 當 fast 到達鏈表尾部時，slow 剛好到達中間節點。
+    while(fast && fast.next){
+        slow = slow.next
+        fast = fast.next.next
+    }
+
+    return slow
+}
+
+console.log(middleNodeTwoPointers(h1))
+console.log(middleNodeTwoPointers(h2))
